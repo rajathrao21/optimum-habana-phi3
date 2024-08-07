92242291e3d421825a356fc41c813133b7c68a5c
rajathrao21 <rajath.rao@intel.com>
2024-08-07 14:59:02 -0700

    Create __init__.py

---
+++ optimum/habana/models/phi3/__init__.py
@@ -0,0 +1,8 @@
+from .configuration_phi3 import Phi3Config
+from .modeling_phi3 import (
+    GaudiPhi3Attention,
+    GaudiPhi3DecoderLayer,
+    GaudiPhi3ForCausalLM,
+    GaudiPhi3Model,
+    gaudi_phi3_rmsnorm_forward,
+)
